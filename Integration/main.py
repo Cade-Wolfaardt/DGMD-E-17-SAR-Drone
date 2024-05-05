958bd189ca07e2ca5d09992c74e6916633b04504
Roger Zeng <roger@cora.org.cn>
2024-05-06 04:08:32 +0800

    Final Report Check-in

---
+++ Integration/main.py
@@ -0,0 +1,209 @@
+import requests
+import os
+import argparse
+import pymavlink_SITL as sitl
+import logging
+
+# Set up basic configuration for logging
+logging.basicConfig(level=logging.DEBUG, filename='sar_drone.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
+
+
+# read in config file
+def read_config_file(config_file_path: str) -> dict:
+    # check to see if file exist
+    if os.path.exists(config_file_path):
+        config_data = {}
+
+        # read in config
+        with open(config_file_path, 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line and not line.startswith('#'):  # ignore comments
+                    key, value = line.split('=', 1)
+                    config_data[key] = value
+
+        logging.info(f"Config File: {config_data}")
+        return config_data
+    else:
+        logging.warning(f"Configuration file {config_file_path} does not exist.")
+
+
+# Flask API preparation
+def set_start(start: tuple, goal: tuple) -> None:
+    """
+    Set the start and goal positions for the mission.
+
+    Sends a request to the server API to assign the start and goal
+    positions for the mission. Prints a success message if the request is
+    successful; otherwise, prints an error message.
+
+    Parameters:
+    - start (tuple): Tuple containing the start position coordinates
+                     (lat, lon, alt).
+    - goal (tuple): Tuple containing the goal position coordinates
+                    (lat, lon, alt).
+    """
+    logging.debug(f"DQN Start from: {start}")
+    logging.debug(f"DQN Go to: {goal}")
+
+    # Construct the URL with start and goal parameters
+    extension = '/assign-start-goal?start={}_{}_{}&goal={}_{}_{}'
+    url = configs['api_server'] + configs['DQN_port'] + extension.format(*start, *goal)
+    logging.debug(f"DQN URL: {url}")
+
+    # Send a GET request to the server API
+    response = requests.get(url)
+    # Check the response status code
+    if response.status_code == 200:
+        logging.info("Set start successfully!")
+    else:
+        logging.info("Error while tryig to start!")
+
+
+# Request path from DQN server API
+def get_path_from_api() -> str:
+    """
+    Retrieve the path information from the API endpoint.
+
+    Makes a GET request to the server API to retrieve the path information.
+    Returns the path content if the request is successful; otherwise, prints
+    an error message and returns None.
+
+    Returns:
+    - str: Path content retrieved from the API.
+    """
+    # Make a GET request to the API endpoint
+#    response = requests.get(api_server + DQN_port + '/get-path')
+    response = requests.get(configs['api_server'] + configs['DQN_port'] + '/get-path') 
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 201:
+        # Extract the path content from the response
+        path_content = response.text
+        return path_content
+    else:
+        # Handle any errors or unexpected status codes
+        logging.error('DQN Path Request Failed! Error Code:', response.status_code)
+        return None
+
+
+# Call the DQN Flask API
+def call_DQN_API(start, goal):
+    # Call the DQN
+    set_start(start, goal)
+    # Call the function to get path content from the API
+    path_content = get_path_from_api()
+    # Check if path content was retrieved successfully
+    if path_content is not None:
+        return path_content
+    else:
+        logging.error('Failed to retrieve DQN path.')
+        raise Exception('DQN path planning failed!')
+
+
+# Call the A* FastAPI
+def call_astar_endpoint(start, goal, obstacles):
+#    url = api_server + AStar_port + '/astar/'
+    url = configs['api_server'] + configs['Astar_port'] + '/astar/'
+    payload = {
+        'start': start,
+        'goal': goal,
+        'obstacles': obstacles,
+    }
+    logging.info(f"A* API: {url}")
+    logging.info(f"A* Request: {payload}")
+
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        logging.info("A* mission received OK")
+        return response.json()
+    else:
+        logging.error("A* Route Request Failed! Status Code:", response.status_code)
+        raise Exception('A* path planning failed!')
+
+def call_rrt_endpoint(start, goal, obstacles):
+#    url = api_server + RRT_port + '/rrt/'
+    url = configs['api_server'] + configs['RRT_port'] + '/rrt/'
+    payload = {
+        'start': start,
+        'goal': goal,
+        'obstacles': obstacles
+    }
+    logging.info(f"RRT API: {url}")
+    logging.info(f"RRT Request: {payload}")
+
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        logging.info("RRT mission plan received OK")
+        return response.json()
+    else:
+        logging.error("RRT Route Request Failed! Status Code:", response.status_code)
+        raise Exception('RRT path planning failed!')
+
+
+# main module for the SAR drone program
+def main():
+    """
+    Main function get path from DQN Network, A* algorithm and RRT algorithm
+
+    Retrieves start and goal coordinates from command-line arguments or uses
+    default values. It then calls API to get mission plans and execute it.
+    """
+
+    # Read Config File
+    global configs
+    configs = read_config_file("config.sar_drone")
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='commands')
+#    parser.add_argument('--help')
+
+    # use mission file or REST API mission query, but not both
+    cmd_group = parser.add_mutually_exclusive_group(required=True)
+    cmd_group.add_argument('--mission')
+    cmd_group.add_argument('--model')
+
+    args = parser.parse_args()
+
+    # Connection configs
+    mission_file = args.mission
+    path_planning_type = args.model
+    connection_string = configs["transport"] + configs["sim_server"] + configs["sim_port"]
+
+    logging.info(f"Connection String: {connection_string}")
+    logging.info(f"Mission File: {mission_file}")
+    logging.info(f"Path Planning Type: {path_planning_type}")
+
+    if mission_file:
+        mission_plan = sitl.read_mission_file(mission_file)
+    else:
+        if path_planning_type == "DQN":
+            # Request DQN mission plan
+            mission_plan = call_DQN_API(configs['start'], configs['goal'])
+        elif path_planning_type == "RRT":
+            # Request RRT mission plan
+            mission_plan = call_rrt_endpoint(configs['start'], configs['goal'], configs['obstacles'])
+        elif path_planning_type == "A*":
+            # Request A* mission plan
+            print("Sorry! A* Algorithm NOT implemented yet")
+#            mission_plan = call_astar_endpoint(configs['start'], configs['goal'], configs['obstacles'])
+        else:
+            print(f"ERROR: Unknown Path Planning Type {path_planning_type}")
+            logging.error(f"ERROR: Unknown Path Planning Type {path_planning_type}")
+            raise Exception('Path planning type not supported')
+
+    # Display Mission Plan
+    # [print(wp) for wp in mission_plan]
+    print(mission_plan)
+
+    # Execute Mission
+    sitl.run_mission(connection_string, mission_plan)
+
+# A* algorithm not working yet. Future development
+#    astar_mission_plan = call_astar_endpoint(start, goal, obstacles)
+#    rrt_mission_plan = call_rrt_endpoint(start, goal, obstacles)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Integration/pymavlink_SITL.py
@@ -0,0 +1,232 @@
+import argparse
+import time
+from pymavlink import mavutil
+import logging
+
+# Set up basic configuration for logging
+logging.basicConfig(level=logging.DEBUG, filename='sar_drone.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
+
+
+# Connect to the Vehicle
+def connect_vehicle(connection_string):
+    if not connection_string:
+        connection_string = 'udp:127.0.0.1:14551'  # default link port
+
+    print('Connecting to vehicle on: %s' % connection_string)
+
+    # Connect to SITL output using the appropriate IP and port
+    vehicle = mavutil.mavlink_connection(connection_string)
+    vehicle.wait_heartbeat()
+    print(f"Heartbeat from system {vehicle.target_system} component {vehicle.target_component}")
+
+    return vehicle
+
+
+# Clear any existing mission
+def clear_mission(vehicle) -> None:
+    vehicle.mav.mission_clear_all_send(vehicle.target_system, vehicle.target_component)
+    while True:
+        msg = vehicle.recv_match(type='MISSION_ACK', blocking=True)
+        if msg.type == mavutil.mavlink.MAV_MISSION_ACCEPTED:
+            print("Mission cleared")
+            break
+
+
+# Read in mission file 
+def read_mission_file(filename: str) -> list:
+    with open(filename, 'r') as file:
+        first_line = file.readline()
+        if not first_line.startswith('QGC WPL 110'):
+            logging.debug(f"First Line Wrong Format: {first_line}")
+            raise Exception('File format not supported')
+
+        mission_items = []
+        for line in file:
+            line = line.strip()
+            if line.startswith('#') or not line:
+                continue
+            mission_items.append(line)
+
+    return mission_items
+
+
+# Upload mission MAVLink command list to drone
+def upload_mission(vehicle, mission):
+    logging.debug(mission)
+    mission_items = []
+
+    # convert mission into MAVLink commands
+    for line in mission:
+        parts = line.split('\t')
+        lat, lon, alt = map(float, parts[8:11])
+        seq = int(parts[0])
+        frame = int(parts[2])
+        command = int(parts[3])
+        mission_items.append(mavutil.mavlink.MAVLink_mission_item_message(
+                vehicle.target_system, vehicle.target_component, seq, frame,
+                command, 0, 0, 0, 0, 0, 0, lat, lon, alt))
+
+        vehicle.mav.mission_count_send(vehicle.target_system, vehicle.target_component, len(mission_items))
+
+        for i, item in enumerate(mission_items):
+            msg = vehicle.recv_match(type='MISSION_REQUEST', blocking=True)
+            vehicle.mav.send(item)
+
+        msg = vehicle.recv_match(type='MISSION_ACK', blocking=True)
+        if msg.type == mavutil.mavlink.MAV_MISSION_ACCEPTED:
+            print("Mission upload SUCCESS!")
+        else:
+            print("Mission upload FAILED!")
+
+        return len(mission_items)
+
+
+# Arm and take off the drone
+def arm_and_takeoff(vehicle, target_altitude):
+    # Switch to GUIDED mode
+    set_mode(vehicle, "GUIDED")
+
+    # Arm vehicle before attempting to take off
+    print("Arming motors")
+    vehicle.mav.command_long_send(
+        vehicle.target_system,
+        vehicle.target_component,
+        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+        0, 1, 0, 0, 0, 0, 0, 0)  # 1 to arm
+
+    # Wait for arming
+    vehicle.motors_armed_wait()
+    print("Motors armed!")
+
+    print("Taking off!")
+    vehicle.mav.command_long_send(
+        vehicle.target_system,
+        vehicle.target_component,
+        mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
+        0, 0, 0, 0, 0, 0, 0, target_altitude)
+
+    # Wait until the vehicle reaches a safe height
+    while True:
+        # Request altitude
+        vehicle.mav.request_data_stream_send(vehicle.target_system, vehicle.target_component, mavutil.mavlink.MAV_DATA_STREAM_POSITION, 1, 1)
+        msg = vehicle.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
+        altitude = msg.alt / 1000.0  # Convert from mm to m
+
+        print("Altitude:", altitude)
+        if altitude > target_altitude * 0.95:  # Just below target, in meters
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+
+# Land the vehicle
+def land_vehicle(vehicle):
+    print("Landing")
+    vehicle.mav.command_long_send(
+        vehicle.target_system,
+        vehicle.target_component,
+        mavutil.mavlink.MAV_CMD_NAV_LAND,
+        0, 0, 0, 0, 0, 0, 0, 0)
+
+    print("Landing command sent")
+
+
+# Change vehicle mode
+def set_mode(vehicle, target_mode):
+    mode_id = vehicle.mode_mapping()[target_mode]
+
+    # Send mode change command to AUTO mode
+    vehicle.mav.command_long_send(
+        vehicle.target_system,
+        vehicle.target_component,
+        mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
+        mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+        mode_id, 0, 0, 0, 0, 0)
+
+    if command_ack(vehicle, mavutil.mavlink.MAV_CMD_DO_SET_MODE):
+        print(f"Mode set to {target_mode}")
+    else:
+        print(f"FAILED to set mode to {target_mode}")
+
+
+# Check GPS fix before changing mode
+def check_gps_fix(vehicle):
+    ack_msg = vehicle.recv_match(type='GPS_RAW_INT', blocking=True)
+    if ack_msg.fix_type >= 3:  # Typically, a fix_type of 3 means a 3D fix
+        print("GPS Fix OK.")
+        return True
+    else:
+        print("Waiting for GPS Fix...")
+        return False
+
+
+# Check status message
+def command_ack(vehicle, cmd_id):
+    while True:
+        ack_msg = vehicle.recv_match(type='COMMAND_ACK', blocking=True, timeout=5)
+        if ack_msg is None:
+            print("Waiting for command acknowledgement...")
+            time.sleep(1)
+            continue
+        elif ack_msg.command == cmd_id:
+            if ack_msg.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
+                return True
+            else:
+                return False
+
+
+def run_mission(connection_string: str, mission_file: str) -> None:
+    # Mission Preparation
+    copter = connect_vehicle(connection_string)
+    clear_mission(copter)
+    mission_cmds = read_mission_file(mission_file)
+    waypoints = upload_mission(copter, mission_cmds)
+
+    while not check_gps_fix(copter):
+        time.sleep(2)
+
+    # Mission Execution
+    arm_and_takeoff(copter, 10)  # initial height 20m
+    time.sleep(5)  # let the copter catches its breath, before ..
+    set_mode(copter, "AUTO")  # execute the mission
+
+    # Monitor mission progress
+    current_wp = 0
+    while True:
+        next_wp = copter.recv_match(type='MISSION_CURRENT', blocking=True, timeout=5).seq
+        if next_wp == current_wp:
+            continue
+
+        # We've reached a new waypoint
+        print(f"Reached waypoint {next_wp}")
+        current_wp = next_wp
+
+        # Have we done all waypoints?
+        if next_wp == waypoints - 1:
+            # Land and finish
+            land_vehicle(copter)
+            print("Mission Completed")
+            break
+        time.sleep(5)
+
+
+# Protected main
+if __name__ == "__main__":
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='commands')
+    parser.add_argument('--connect')
+    parser.add_argument('--mission')
+    parser.add_argument('--model')
+    args = parser.parse_args()
+
+    # Connect to quadcopter
+    connection_string = args.connect
+    mission_file = args.mission
+    ppmodel = args.model
+
+    print(f"Connection String: {connection_string}")
+    print(f"Mission Data: {mission_file}")
+    print(f"Path Planning Type: {ppmodel}")
+
+    run_mission(connection_string, read_mission_file(mission_file))
